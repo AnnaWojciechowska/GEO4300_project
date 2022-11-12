50f5706918811294ff6e8a9f3642727d7db59869
Anna Wojciechowska <anna.wojciechowska@qt.io>
2022-11-12 12:19:51 +0100

    Nan values handling - removed and stored as csv

--- reading_data/influx_db/server_download_and_store.py
+++ reading_data/influx_db/server_download_and_store.py
@@ -10,6 +10,7 @@ from influxdb import InfluxDBClient
 from influxdb.exceptions import InfluxDBClientError, InfluxDBServerError
 import requests
 import io
+import os
 
 from influx_tools import *
 
@@ -46,7 +47,9 @@ def extract_header_and_data(lines):
             header_dict[res[0].strip()] = res[1].strip() 
     return header_dict, data
 
-def parse_response(res):
+def parse_response(res, LOGGER, logs_dir):
+    #res (ponse) is pandas dataframe
+    correct_check = True
     response_text = res.text.split('\r\n')
     header, data = extract_header_and_data(response_text)
     df = pd.read_csv(io.StringIO(data), sep='\s+', usecols = ['timestamp', 'level'])
@@ -54,9 +57,23 @@ def parse_response(res):
     df['lat'] = header['Latitude']
     df['lon'] = header['Longitude']
     df['ref'] = header['Reference level']
-    df['level'] = df['level'].fillna(0)
+    # tutajcorrect that tooo!!!
     df['timestamp'] = df['timestamp'].apply(lambda x: x.split('+')[0])
     df['timestamp'] = df['timestamp'].apply(lambda x: dt.strptime(x,"%Y-%m-%dT%H:%M:%S"))
+  
+
+    # dealing with nan values:
+    # we extract them from main df and keep in seprate df
+    nan_df = df[df['level'].isna()]
+    if nan_df.shape[0] > 0:
+        print("nans!")
+        df.drop(nan_df.index.to_list(), inplace=True)
+        # choose what is better way to handle: 
+        nan_csv_filenamane = os.path.join(logs_dir, df.iloc[0]['site'] + ".csv")
+        nan_df.to_csv(nan_csv_filenamane, index=False)
+        LOGGER.info("Nan values: {}".format(nan_df.shape[0]))
+        LOGGER.info(nan_df.to_csv())
+
     return df
 
 
@@ -93,8 +110,9 @@ LOGGER.info("end script")
 
 
 tide_stations = pd.read_csv('data/tide_stations_loc.csv')
-start_date = '2022-01-01'
-end_date = '2022-10-03'
+start_date = '2022-01-01' # yyyy-mm-dd
+#tutaj
+end_date = '2022-10-01'
 data_type = 'OBS'
 #url= url_template.format(lat, lon, datatype, place, start_date, end_date)
 url_template = 'http://api.sehavniva.no/tideapi.php?tide_request=locationdata&lat={}&lon={}\
@@ -106,6 +124,7 @@ urls = []
 
 for i, row in tide_stations.iterrows():
     url = url_template.format(row['latitude'], row['longitude'], data_type, row['code'], start_date, end_date)
+    print(url)
     urls.append(url)
     
 columns = ['timestamp', 'level', 'site', 'lat', 'lon', 'ref']
@@ -113,13 +132,16 @@ df = pd.DataFrame(columns=columns)
 i = 0
 for url in urls:
     station_code = tide_stations['code'].iloc[i]
+    print("processing: {} {}".format(i, station_code))
     i += 1
-    LOGGER.info("getting data from: ".format(station_code))
+    LOGGER.info("getting data from: {}".format(station_code))
     res = requests.get(url)
     if res.status_code != 200:
         print("ups, rquest error for ", url)
     else:
-        temp_df = parse_response(res)
+        #print(res.text)
+        #print("tutaj")
+        temp_df = parse_response(res, LOGGER, log_dir)
         df = pd.concat([df, temp_df])
 df.reset_index(drop=True, inplace=True)
 
